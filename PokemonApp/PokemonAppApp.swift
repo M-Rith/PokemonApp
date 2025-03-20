44bcd2a51c49b65cbff1cd8aee50e94aae75779b
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-20 23:14:28 +0700

    start working on Pokemon App

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by AppleD0g on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
